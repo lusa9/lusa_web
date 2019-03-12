7017043308be0e1e041f0020fe90151af2c57fe8
Luka Ušalj <luka.usalj@gmail.com>
2019-03-12 16:42:17 +0100

    Updates

---
+++ precache-manifest.ed7e318715d58537e0c20cdd6a9bd799.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "10253ca96e334e5ea08b",
+    "url": "/static/css/main.1a703817.chunk.css"
+  },
+  {
+    "revision": "10253ca96e334e5ea08b",
+    "url": "/static/js/main.d5a4519e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "12ea7ae47ce8e8b8a455",
+    "url": "/static/js/2.950f48ee.chunk.js"
+  },
+  {
+    "revision": "02549f990c62adb7df782925ae3875e8",
+    "url": "/index.html"
+  }
+];
