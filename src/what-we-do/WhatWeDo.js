5f34ed7369dac91254e53178394538210ca89028
Luka Ušalj <luka.usalj@gmail.com>
2019-03-15 19:31:41 +0100

    implemented what we do for various screen sizes

--- src/what-we-do/WhatWeDo.js
+++ src/what-we-do/WhatWeDo.js
@@ -4,19 +4,17 @@ import "./WhatWeDo.css"
 
 export class WhatWeDo extends Component {
   render() {
-    const BigSpace = () => <Space height={270}/>
-    const SmallSpace = () => <Space height={80}/>
     return (
         <div className="what-we-do-container">
-          <BigSpace/>
+          <div className="bigspace"/>
           <img className="image" src="/assets/macbook.png"/>
-          <SmallSpace/>
+          <div className="smallspace"/>
           <text className="what-we-do-item-description">Web Frontend using HTML & CSS with Angular, React or Vue.js</text>
-          <BigSpace/>
+          <div className="bigspace"/>
           <img className="image" src="/assets/phones.png"/>
-          <SmallSpace/>
+          <div className="smallspace"/>
           <text className="what-we-do-item-description">Cross-Platform Mobile development with React Native</text>
-          <BigSpace/>
+          <div className="bigspace"/>
         </div>
     )
   }
