6b58e8f4cc2d56d27a25443c5bed1e1ec160479a
Luka Ušalj <luka.usalj@gmail.com>
2019-03-16 22:27:46 +0100

    redesigned published apps for desktops

--- src/recent-work/RecentWork.js
+++ src/recent-work/RecentWork.js
@@ -6,16 +6,15 @@ export class RecentWork extends Component {
   render() {
     return (
         <div className="recent-work-container">
-          <div className="bigspace"/>
+          <div className="title-space"/>
           <text className="title">Published Apps</text>
-          <div className="recent-work-item-space"/>
+          <div className="title-space"/>
           <Project 
             imagesrc="assets/croparkingapp.png" 
             name="CroParking"
             description="Simple & Fast parking SMS Generator built in React Native"
             googleplaylink="https://play.google.com/store/apps/details?id=com.lusa.croparking&hl=en"
           />
-          <div className="bigspace"/>
         </div>
     )
   }
@@ -24,14 +23,16 @@ export class RecentWork extends Component {
 class Project extends Component {
   render() {
     return (
-        <div className="published-apps-item-container">
-          <img className="published-apps-item-image" src={this.props.imagesrc}/>
-          <div className="smallspace"/>
-          <div className="published-apps-croparking-description-container">
-            <text className="RecentWork-title">{this.props.name}</text>
-            <text className="project-description">Simple & Fast parking SMS Generator built in React Native</text>
+        <div className="project-container">
+          <img className="project-image" src={this.props.imagesrc}/>
+          <div className="project-image-details-space"/>
+          <div className="project-details-container">
+            <text className="project-title">{this.props.name}</text>
+            <div className="project-title-description-space"/>
+            <text className="project-description">Simple & Fast parking SMS Generator built with React Native</text>
+            <div className="project-description-buttons-space"/>
+            <a className="GooglePlayButton" href={this.props.googleplaylink}><img src="/assets/google_play.png"/></a>
             <div className="smallspace"/>
-            <a href={this.props.googleplaylink}><img className="GooglePlayButton" src="/assets/google_play.png"/></a>
           </div>
         </div>
     )
