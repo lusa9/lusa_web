478df190f4b406cd4fdaa89a8f7e664c2cc4fd4c
Luka Ušalj <luka@Lukas-iMac.local>
2019-03-25 15:09:16 +0100

    Updates

---
+++ precache-manifest.6f0ea266bddd4b14dd4ec26583f6d938.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4b1af0c3aca86b39d20c",
+    "url": "/static/css/main.46844400.chunk.css"
+  },
+  {
+    "revision": "4b1af0c3aca86b39d20c",
+    "url": "/static/js/main.f434803f.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "1815851389d014e92dba",
+    "url": "/static/js/2.029bc5ae.chunk.js"
+  },
+  {
+    "revision": "23ecf78f8655314a88d9434af10d1e53",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.39d21ce321c12442d4718c09d78704e5.js"
+  "/precache-manifest.6f0ea266bddd4b14dd4ec26583f6d938.js"
 );
 
 workbox.clientsClaim();
